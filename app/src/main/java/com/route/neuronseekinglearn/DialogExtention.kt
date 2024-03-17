d7ab568cc6f91fdadc2683571a086b81d4de334b
Hosny-Hany <134730010+Hosny-Hany@users.noreply.github.com>
2024-03-17 15:16:13 +0300

    +Splash_Screen +SignUp_Screen +Activity +Vm +SignIn_Screen +Home_Screen +RoadMap_Screen +Profile_Screen +Help_Screen

--- app/src/main/java/com/route/neuronseekinglearn/DialogExtention.kt
+++ app/src/main/java/com/route/neuronseekinglearn/DialogExtention.kt
@@ -2,31 +2,35 @@ package com.route.neuronseekinglearn
 
 import android.app.Activity
 import android.app.AlertDialog
-import android.content.DialogInterface
 import androidx.fragment.app.Fragment
 
 fun Fragment.showDialog(
     message: String,
     posMessage: String? = null,
-    posAction: DialogInterface.OnClickListener? = null,
+    posAction: OnDialogActionClick? = null,
     negMessage: String? = null,
-    negAction: DialogInterface.OnClickListener? = null
+    negAction: OnDialogActionClick? = null
 
 ): AlertDialog {
     val dialog = AlertDialog.Builder(context)
     dialog.setMessage(message)
-    if (posAction != null) {
-        dialog.setPositiveButton(posMessage, posAction)
+    if (posMessage != null) {
+        dialog.setPositiveButton(posMessage)
+        { dialog, id ->
+            dialog.dismiss()
+            posAction?.OnActionClick()
+        }
+    }
+    if (negMessage != null) {
+        dialog.setNegativeButton(negMessage)
+        { dialog, id ->
+            dialog.dismiss()
+            negAction?.OnActionClick()
+        }
 
     }
-    if (negAction != null) {
-        dialog.setNegativeButton(negMessage, posAction)
-
-    }
-
     return dialog.show()
 }
-
 
 fun Activity.showDialog(
     message: String,
@@ -37,19 +41,21 @@ fun Activity.showDialog(
     isCancellable: Boolean = true
 ): AlertDialog {
     val dialog = AlertDialog.Builder(this)
-    dialog.setMessage(message)
-    if (posAction != null) {
-        dialog.setPositiveButton(posMessage) { dialog, _ ->
-            posAction.OnActionClick()
+    if (posMessage != null) {
+        dialog.setPositiveButton(posMessage)
+        { dialog, id ->
+            dialog.dismiss()
+            posAction?.OnActionClick()
+        }
+    }
+    if (negMessage != null) {
+        dialog.setNegativeButton(negMessage)
+        { dialog, id ->
+            dialog.dismiss()
+            negAction?.OnActionClick()
         }
 
-    }
-    if (negAction != null) {
-        dialog.setNegativeButton(negMessage) { dialog, _ ->
-            negAction.OnActionClick()
-        }
     }
     dialog.setCancelable(isCancellable)
-
     return dialog.show()
 }
