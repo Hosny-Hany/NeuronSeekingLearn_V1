4c83e34d14b1cd0b6d2d8e394173d4886cd61a1b
Hosny-Hany <134730010+Hosny-Hany@users.noreply.github.com>
2024-05-05 17:11:08 +0300

    Solve Login issue ok try to solve my issue show this change last change for me

--- app/src/main/java/com/route/neuronseekinglearn/home/tabs/HomeActivity.kt
+++ app/src/main/java/com/route/neuronseekinglearn/home/tabs/HomeActivity.kt
@@ -1,14 +1,15 @@
 package com.route.neuronseekinglearn.home.tabs
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import com.route.neuronseekinglearn.R
 import com.route.neuronseekinglearn.databinding.ActivityHomeBinding
 import com.route.neuronseekinglearn.home.tabs.home.HomeFragment
+import com.route.neuronseekinglearn.home.tabs.menu.ProfileFragment
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var viewBinding : ActivityHomeBinding
+    private lateinit var viewBinding: ActivityHomeBinding
     private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +18,14 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         handleSidebar()
         navigateToHomeFragment()
+        navigateToProfileFragment()
+    }
+
+    private fun navigateToProfileFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, ProfileFragment())
+            .addToBackStack(null)
+            .commit()
     }
 
     private fun navigateToHomeFragment() {
@@ -25,6 +34,7 @@ class HomeActivity : AppCompatActivity() {
             .addToBackStack(null)
             .commit()
     }
+
     private fun handleSidebar() {
         toggle = ActionBarDrawerToggle(
             this, viewBinding.drawerLayout,
